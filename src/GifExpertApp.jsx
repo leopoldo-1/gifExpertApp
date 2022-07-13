bacc6ec8fe235a1ddd4cf4b0f20dbe1f3dd762ac
leopoldo <leopoldo.miranda18@tectijuana.edu.mx>
2022-07-12 23:39:16 -0700

    prime-react added

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
+import { Button } from 'primereact/button';
+
+import PrimeReact from 'primereact/api';
+
 
 export const GiftExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon ball' ])
+  const [selectCategory, setSelectCategory] = useState(null)
+
+  PrimeReact.ripple  = true;
+  const onAddCategory = () => {
+    // console.log(categories);
+    setCategories([ 'Valorant', ...categories ])
+    // setCategories( cat => [...cat, 'Valorant'] );
+  }
+
   return (
     <>
-      <h1>Gif Expert App</h1>
-      <p>LfUzyKCYMDP4i6KcFLjVCQJmPbg4ulk0</p>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* input */}
+
+      {/* listado de items/gifs */}
+      <Button onClick={ onAddCategory } label="Add" icon="pi pi-plus" />
+      {/* <button onClick={ onAddCategory }>Add</button> */}
+
+      <ol>
+        { categories.map( category => {
+            return <li key={ category }>{ category }</li>
+          })
+        }
+      </ol>
+        {/* gif item */}
+
+      <p>  LfUzyKCYMDP4i6KcFLjVCQJmPbg4ulk0</p>
     </>
   )
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GiftExpertApp } from './GifExpertApp';
+import "primereact/resources/themes/saga-blue/theme.css";  //theme
+import 'primeicons/primeicons.css';
 
+import { GiftExpertApp } from './GifExpertApp';
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
